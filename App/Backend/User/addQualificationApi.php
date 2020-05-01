090eb977285ae153d75fd6d9ddf5e0d38b18e586
gaveenkith-ucsc <gaveen.kith@worldmosquito.org>
2020-05-01 15:57:11 +0530

    vacancy management seeker part finished at registration wizard

--- App/Backend/User/addQualificationApi.php
+++ App/Backend/User/addQualificationApi.php
@@ -12,6 +12,11 @@ $completedyear = $user->completedyear;
 
 $conn = new mysqli("localhost", "root", "", "jobsmanagement");
 $result = $conn->query("insert into qualification (studentid,sector,type,institute,certificateno,completedyear) values('$nic_no','$sector','$type','$institute','$certificateno','$completedyear')");
+$results2 = $conn->query("select * from vacancy where sector='$sector' and qualification_type='$type' and expiration_date>=CURDATE()");
+while ($result2 = $results2->fetch_array(MYSQLI_ASSOC)) {
+    $vacancy_id = $result2["vacancy_id"];
+    $result3 = $conn->query("insert into seeker_vacancy (seeker_id,vacancy_id,applied_date,status) values('$nic_no','$vacancy_id',CURDATE(),'1')");
+}
 $outp = "";
 if ($result) {
     $status = "ok";
